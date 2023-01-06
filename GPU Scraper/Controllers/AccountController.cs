c584b80590363c6b722bc34a0dcfac07776f03df
Jacek Fedorowicz <58124041+jacekfedorowiczz@users.noreply.github.com>
2023-01-07 00:08:16 +0100

    07.01.2023 - uzupełnienie kontrolera konta użytkownika, dodanie cachowania zapytania pobierającego plik json z kartami graficznymi

--- GPU Scraper/Controllers/AccountController.cs
+++ GPU Scraper/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GPUScraper.Models.Models;
+using GPUScraper.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GPUScraper.Controllers
 {
@@ -6,9 +8,25 @@ namespace GPUScraper.Controllers
     [Route("account")]
     public class AccountController : ControllerBase
     {
-        public AccountController()
-        {
+        private readonly IAccountService _accountService;
 
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("/register")]
+        public ActionResult RegisterUser([FromBody]RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+            return Ok();
+        }
+
+        [HttpPost("/login")]
+        public ActionResult<string> Login([FromBody]LoginUserDto dto)
+        {
+            var token = _accountService.GenerateJwt(dto);
+            return Ok(token);
         }
     }
 }
