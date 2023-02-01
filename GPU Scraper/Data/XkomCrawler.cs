adfbf6155cb287ebc9321b6939bb214810e6c404
Jacek Fedorowicz <58124041+jacekfedorowiczz@users.noreply.github.com>
2023-02-01 23:53:23 +0100

    01.02.2023 Refaktor crawlera - hermetyzacja logiki odpowiadającej za przeszukiwanie stron w celu znalezienia produktów

--- GPU Scraper/Data/XkomCrawler.cs
+++ GPU Scraper/Data/XkomCrawler.cs
@@ -8,25 +8,69 @@ using System;
 using System.Globalization;
 using Newtonsoft.Json;
 using GPU_Scraper.Data.Contracts;
+using System.Security.Policy;
 
 namespace GPU_Scraper.Data
 {
     public class XkomCrawler : ICrawler
     {
+        const string XKomBaseURL = "https://www.x-kom.pl/g-5/c/345-karty-graficzne.html?f1702-uklad-graficzny=24826-amd-radeon&f1702-uklad-graficzny=24827-nvidia-geforce&f1702-uklad-graficzny=262522-intel-arc";
+        HtmlWeb Web = new HtmlWeb();
+        NumberFormatInfo nfi = new CultureInfo("pl-PL", false).NumberFormat;
+
         public async Task<List<Product>> CrawlProducts()
         {
-            const string XKomBaseURL = "https://www.x-kom.pl/g-5/c/345-karty-graficzne.html?f1702-uklad-graficzny=24826-amd-radeon&f1702-uklad-graficzny=24827-nvidia-geforce&f1702-uklad-graficzny=262522-intel-arc";
-            var web = new HtmlWeb();
-            var document = web.Load(XKomBaseURL);
-            var Urls = new List<string>() { XKomBaseURL };
+            var urls = GetUrls();
+            var products = GetProducts(urls);
+
+            if (products == null)
+            {
+                throw new Exception();
+            }
+
+            return products;
+
+        }
+
+        private List<Product> GetProducts(List<string> urls)
+        {
+            var gpusList = new List<Product>();
+            NumberFormatInfo nfi = new CultureInfo("pl-PL", false).NumberFormat;
+
+            foreach (var url in urls)
+            {
+                var page = Web.Load(url);
+                var products = page.QuerySelectorAll(".gyHdpL");
+
+                foreach (var product in products)
+                {
+                    var productName = String.Join(' ', product.QuerySelector("a > h3").Attributes["title"].Value.ToString().Split(' ').Skip(3)).Trim();
+                    var productPrice = decimal.Parse(product.QuerySelector(".gAlJbD > span.guFePW").InnerText.ToString(nfi).Replace("zł", "").Replace("od", ""));
+
+                    gpusList.Add(
+                        new Product
+                        {
+                            Name = productName,
+                            Price = productPrice,
+                            Shop = "X-Kom"
+                        });
+                }
+            }
+            return gpusList;
+        }
+
+        private List<string> GetUrls()
+        {
+            var document = Web.Load(XKomBaseURL);
+            var urls = new List<string>() { XKomBaseURL };
 
             if (document.QuerySelector("a.kGuktN").Attributes["href"].Value != null)
             {
                 var nextPageUrl = "https://x-kom.pl/" + document.QuerySelector("a.kGuktN").Attributes["href"].Value.ToString();
                 while (nextPageUrl != null)
                 {
-                    Urls.Add(nextPageUrl);
-                    var newDocument = web.Load(nextPageUrl);
+                    urls.Add(nextPageUrl);
+                    var newDocument = Web.Load(nextPageUrl);
                     if (newDocument.QuerySelector(".ieNAbN") != null)
                     {
                         break;
@@ -38,35 +82,7 @@ namespace GPU_Scraper.Data
                 }
             }
 
-            var gpusList = new List<Product>();
-            NumberFormatInfo nfi = new CultureInfo("pl-PL", false).NumberFormat;
-
-            foreach (var url in Urls)
-            {
-                var page = web.Load(url);
-                var products = page.QuerySelectorAll(".gyHdpL");
-
-                foreach (var product in products)
-                {
-                    var productName = String.Join(' ', product.QuerySelector("a > h3").Attributes["title"].Value.ToString().Split(' ').Skip(3)).Trim();
-                    var productPrice = double.Parse(product.QuerySelector(".gAlJbD > span.guFePW").InnerText.ToString(nfi).Replace("zł", "").Replace("od", ""));
-
-                    gpusList.Add(
-                        new Product
-                        {
-                            Name = productName,
-                            Price = productPrice,
-                            Shop = "X-Kom"
-                        });
-                }
-            }
-
-            if (!gpusList.Any())
-            {
-                throw new Exception();
-            }
-
-            return gpusList;
+            return urls;
         }
     }
 }
--- GPUScraper.Models/Models/Product.cs
+++ GPUScraper.Models/Models/Product.cs
@@ -9,7 +9,7 @@ namespace GPUScraper.Models.Models
     public class Product
     {
         public string Name { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string Shop { get; set; }
     }
 }
